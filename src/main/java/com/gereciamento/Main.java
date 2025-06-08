f068042da926005dda817d659d13da2cf4e0186e
Leonardo Lopes Honda <leozinhohondams@gmail.com>
2025-06-08 15:37:25 -0400

    mend

--- src/main/java/com/gereciamento/Main.java
+++ src/main/java/com/gereciamento/Main.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import javax.swing.JOptionPane;
 
+import com.gereciamento.db.AviaoDB;
 import com.gereciamento.db.PassageiroDB;
+import com.gereciamento.models.Aviao;
 import com.gereciamento.models.Passageiro;
 
 public class Main {
@@ -23,35 +25,36 @@ public class Main {
   }
 
   public static void main(String[] args) {
-    List<Passageiro> listaPassageiros = populateList(null);
-    Passageiro passageiro = new Passageiro();
+    // List<Passageiro> listaPassageiros = populateList(null);
+    // Passageiro passageiro = new Passageiro();
+    // int opt = -1;
+    // do {
+    //   opt = Integer.parseInt(JOptionPane.showInputDialog(null,
+    //       "Selecione uma opcao: \n\n ==> Passageiro" + "\n1- Adicionar Passageiro \n" + "2- Editar Passageiros \n"
+    //           + "3- Deletar Passageiro \n" + "4- Listar Passageiros \n\n" + "0- Sair \n\n"));
+    //   switch (opt) {
+    //   case 1:
+    //     passageiro.create(listaPassageiros);
+    //     break;
+    //   case 2:
+    //     passageiro.update(listaPassageiros);
+    //     break;
+    //   case 3:
+    //     passageiro.remove(listaPassageiros);
+    //     break;
+    //   case 4:
+    //     showList(passageiro, listaPassageiros);
+    //     break;
+    //   case 0:
+    //     JOptionPane.showMessageDialog(null, "Saindo do sistema, obrigado pela preferencia");
+    //     break;
+    //   default:
+    //     JOptionPane.showMessageDialog(null, "Opcao Invalida, tente novamente");
+    //     break;
+    //   }
+    // } while (opt != 0);
 
-    int opt = -1;
-
-    do {
-      opt = Integer.parseInt(JOptionPane.showInputDialog(null,
-          "Selecione uma opcao: \n\n ==> Passageiro" + "\n1- Adicionar Passageiro \n" + "2- Editar Passageiros \n"
-              + "3- Deletar Passageiro \n" + "4- Listar Passageiros \n\n" + "0- Sair \n\n"));
-      switch (opt) {
-      case 1:
-        passageiro.create(listaPassageiros);
-        break;
-      case 2:
-        passageiro.update(listaPassageiros);
-        break;
-      case 3:
-        passageiro.remove(listaPassageiros);
-        break;
-      case 4:
-        showList(passageiro, listaPassageiros);
-        break;
-      case 0:
-        JOptionPane.showMessageDialog(null, "Saindo do sistema, obrigado pela preferencia");
-        break;
-      default:
-        JOptionPane.showMessageDialog(null, "Opcao Invalida, tente novamente");
-        break;
-      }
-    } while (opt != 0);
+    AviaoDB aviaoDB = new AviaoDB();
+    Aviao aviao = new Aviao();
   }
 }
