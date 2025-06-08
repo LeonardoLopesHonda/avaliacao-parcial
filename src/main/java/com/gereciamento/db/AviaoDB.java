8208f0e864b107ea2827ca43829a9d1a14f5342e
Leonardo Lopes Honda <leozinhohondams@gmail.com>
2025-06-08 15:36:48 -0400

    feat: add `aviao` database `crud` frunctioms

---
+++ src/main/java/com/gereciamento/db/AviaoDB.java
@@ -0,0 +1,56 @@
+package com.gereciamento.db;
+
+import java.util.List;
+
+import com.gereciamento.models.Aviao;
+
+public class AviaoDB {
+  protected Repository repository;
+
+  public AviaoDB() {
+    this.repository = Repository.getInstance();
+  }
+
+  public void create(Aviao aviao) {
+    try {
+      repository.entityManager.getTransaction().begin();
+      repository.entityManager.persist(aviao);
+      repository.entityManager.getTransaction().commit();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  public List<Aviao> read() {
+    return repository.entityManager.createQuery("SELECT a FROM aviao a", Aviao.class).getResultList();
+  }
+
+  public void update(Aviao aviao, Long id) {
+    try {
+      repository.entityManager.getTransaction().begin();
+      aviao.setId(id);
+      repository.entityManager.merge(aviao);
+      repository.entityManager.getTransaction().commit();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  public void delete(Long id) {
+    Aviao aviao = repository.entityManager.find(Aviao.class, id);
+    try {
+      repository.entityManager.getTransaction().begin();
+      repository.entityManager.remove(aviao);
+      repository.entityManager.getTransaction().commit();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  public Aviao findOneById(Long id) {
+    List<Aviao> list = repository.entityManager.createQuery("SELECT a FROM Aviao a WHERE id = " + id, Aviao.class)
+        .getResultList();
+
+    return list.getFirst();
+  }
+}
